588d08aeb5e62759f39a240c058f970c89bbd431
mattexe <teomarelli2@gmail.com>
2024-09-23 15:43:06 +0200

    login activity with improvements

--- app/src/main/java/com/example/loginui/LoginActivity.kt
+++ app/src/main/java/com/example/loginui/LoginActivity.kt
@@ -10,18 +10,18 @@ import android.text.InputType
 import android.view.MotionEvent
 import android.widget.EditText
 import android.widget.Toast
-import com.example.loginui.databinding.ActivityLoginBinding
+import com.example.loginui.databinding.LoginActivityBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: LoginActivityBinding
     private lateinit var firebaseAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = LoginActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
 
@@ -56,7 +56,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun checkLoginCredential(){
-        binding.loginbutton.setOnClickListener{
+        binding.loginButton.setOnClickListener{
             val email = binding.edUserName.text.toString()
             val password = binding.edPassword.text.toString()
 
