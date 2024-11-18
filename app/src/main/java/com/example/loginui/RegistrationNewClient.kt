8eb1a1d38f9c9bc1d138ec32eff9c7f1f78fd825
mattexe <teomarelli2@gmail.com>
2024-11-18 22:19:47 +0100

    add code to register new client

--- app/src/main/java/com/example/loginui/RegistrationNewClient.kt
+++ app/src/main/java/com/example/loginui/RegistrationNewClient.kt
@@ -1,9 +1,13 @@
 package com.example.loginui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.text.toLowerCase
 import com.example.loginui.databinding.ActivityRegistrationBinding
 import com.example.loginui.databinding.RegistrationNewClientBinding
+import com.example.loginui.models.ClientModel
 import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationNewClient : AppCompatActivity()  {
@@ -19,10 +23,23 @@ class RegistrationNewClient : AppCompatActivity()  {
         db = WorkoutDatabaseHelper(this)
         firebaseAuth = FirebaseAuth.getInstance()
 
+        registerNewClient()
     }
 
     //function
     private fun registerNewClient(){
+        binding.registerNewClient.setOnClickListener{
+            val name = binding.editClientName.text.toString().trim()
+            val surname = binding.editClientSurname.text.toString().trim()
 
+            if(name.isNotBlank() && surname.isNotBlank()){
+                val clientModel = ClientModel("0", name, surname, 0, "s")
+                db.insertClient(clientModel)
+                Toast.makeText(this, "New client registered!", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
