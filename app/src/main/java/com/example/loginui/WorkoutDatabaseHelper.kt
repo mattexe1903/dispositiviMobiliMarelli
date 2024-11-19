cd648f9a02e773c9939548bdfb847940d3667c73
mattexe <teomarelli2@gmail.com>
2024-11-19 18:18:44 +0100

    new database management

--- app/src/main/java/com/example/loginui/WorkoutDatabaseHelper.kt
+++ app/src/main/java/com/example/loginui/WorkoutDatabaseHelper.kt
@@ -11,216 +11,156 @@ import com.example.loginui.models.ExerciseModel
 import com.example.loginui.models.PersonalTrainerModel
 import com.example.loginui.models.TrainingDetailsModel
 import com.example.loginui.models.TrainingModel
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 class WorkoutDatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     companion object{
         private const val DATABASE_NAME = "ptl.db"
         private const val DATABASE_VERSION = 1
+        private const val DATABASE_PATH = "/data/data/com.example.loginui/databases/"
+    }
 
-        //table
-        private const val TABLE_RPR = "rpr"
-        private const val TABLE_CLIENT = "client"
-        private const val TABLE_OPERATOR = "operator"
-        private const val TABLE_EXERCISE ="exercise"
-        private const val TABLE_TRAINING = "training"
-        private const val TABLE_TRAININGDETAILS ="trainingDetails"
+    private val dbFilePath: String = DATABASE_PATH + DATABASE_NAME
+    private val appContext: Context = context.applicationContext
 
-        //Column RPR table
-        private const val COLUMN_RPR_ID = "id"
-        private const val COLUMN_RPR_MOOD = "mood"
-        private const val COLUMN_RPR_SLEEP = "sleep"
-        private const val COLUMN_RPR_ENERGY = "energy"
-        private const val COLUMN_RPR_DOMS = "doms"
-        private const val COLUMN_RPR_AVG = "avg"
-        private const val COLUMN_RPR_BORG = "borg"
-        private const val COLUMN_RPR_CLIENT = "client"
-        private const val COLUMN_RPR_TRAINING = "training"
-
-        //Column client table
-        private const val COLUMN_CLIENT_ID = "id"
-        private const val COLUMN_CLIENT_NAME = "name"
-        private const val COLUMN_CLIENT_SURNAME = "surname"
-
-        //Column operator table
-        private const val COLUMN_OP_UID = "uid"
-        private const val COLUMN_OP_NAME = "name"
-
-        //column exercise table
-        private const val COLUMN_EX_ID= "id"
-        const val COLUMN_EX_NAME = "name"
-        private const val COLUMN_EX_CATEGORY = "category"
-        private const val COLUMN_EX_PHYSICAL_GOAL= "physicalGoal"
-        private const val COLUMN_EX_TYPE= "type"
-
-        //column training table
-        private const val COLUMN_TRAINING_ID = "id"
-        private const val COLUMN_TRAINING_DATE = "date"
-        private const val COLUMN_TRAINING_DURATION ="duration"
-        private const val COLUMN_TRAINING_CLIENT = "client"
-        private const val COLUMN_TRAINING_PT= "operator"
-        private const val COLUMN_TRAINING_WNUMBER = "workoutNumber"
-
-        //column training details table
-        private const val COLUMN_TD_ID="id"
-        private const val COLUMN_TD_REPS = "reps"
-        private const val COLUMN_TD_SETS ="sets"
-        private const val COLUMN_TD_WEIGHT= "weight"
-        private const val COLUMN_TD_TRAINING="training"
-        private const val COLUMN_TD_EXERCISE= "exercise"
-        private const val COLUMN_TD_NOTE="note"
-        private const val COLUMN_TD_EXTIME="executionTime"
-        private const val COLUMN_TD_BORG="borg"
-
-        //creation table
-        private const val CREATETABLERPR = "CREATE TABLE $TABLE_RPR(" +
-                "$COLUMN_RPR_ID INTEGER PRIMARY KEY, " +
-                "$COLUMN_RPR_DOMS TEXT," +
-                "$COLUMN_RPR_MOOD TEXT," +
-                "$COLUMN_RPR_ENERGY TEXT," +
-                "$COLUMN_RPR_SLEEP TEXT," +
-                "$COLUMN_RPR_BORG TEXT," +
-                "$COLUMN_RPR_AVG TEXT," +
-                "$COLUMN_RPR_CLIENT TEXT, "+
-                "$COLUMN_RPR_TRAINING TEXT)"
-
-        private const val CREATETABLECLIENT = "CREATE TABLE $TABLE_CLIENT(" +
-                "$COLUMN_CLIENT_ID INTEGER PRIMARY KEY," +
-                "$COLUMN_CLIENT_NAME TEXT, " +
-                "$COLUMN_CLIENT_SURNAME TEXT)"
-
-        private const val CREATETABLEOPERATOR = "CREATE TABLE $TABLE_OPERATOR(" +
-                "$COLUMN_OP_UID TEXT PRIMARY KEY,"+
-                "$COLUMN_OP_NAME TEXT)"
-
-        private const val CREATETABLEEXERCISE = "CREATE TABLE $TABLE_EXERCISE("+
-                "$COLUMN_EX_ID INTEGER PRIMARY KEY, "+
-                "$COLUMN_EX_NAME TEXT, "+
-                "$COLUMN_EX_CATEGORY TEXT,"+
-                "$COLUMN_EX_PHYSICAL_GOAL TEXT, "+
-                "$COLUMN_EX_TYPE TEXT)"
-
-        private const val CREATETABLETRAINING = "CREATE TABLE $TABLE_TRAINING("+
-                "$COLUMN_TRAINING_ID INTEGER PRIMARY KEY, "+
-                "$COLUMN_TRAINING_DATE TEXT,"+
-                "$COLUMN_TRAINING_DURATION TEXT,"+
-                "$COLUMN_TRAINING_CLIENT TEXT," +
-                "$COLUMN_TRAINING_PT TEXT, "+
-                "$COLUMN_TRAINING_WNUMBER INTEGER)"
-
-        private const val CREATETABLETD = "CREATE TABLE $TABLE_TRAININGDETAILS("+
-                "$COLUMN_TD_ID INTEGER PRIMARY KEY, "+
-                "$COLUMN_TD_REPS TEXT, "+
-                "$COLUMN_TD_SETS TEXT, "+
-                "$COLUMN_TD_WEIGHT TEXT, "+
-                "$COLUMN_TD_EXTIME TEXT, "+
-                "$COLUMN_TD_EXERCISE TEXT, "+
-                "$COLUMN_TD_TRAINING TEXT, "+
-                "$COLUMN_TD_BORG TEXT, "+
-                "$COLUMN_TD_NOTE TEXT)"
-
+    init{
+        copyDatabaseIfNeeded()
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(CREATETABLERPR)
-        db?.execSQL(CREATETABLECLIENT)
-        db?.execSQL(CREATETABLEOPERATOR)
-        db?.execSQL(CREATETABLEEXERCISE)
-        db?.execSQL(CREATETABLETRAINING)
-        db?.execSQL(CREATETABLETD)
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        val dropTableClientQuery = "DROP TABLE IF EXISTS $TABLE_CLIENT"
-        val dropTableRPRQuery = "DROP TABLE IF EXISTS $TABLE_RPR"
-        val dropTableOpQuery = "DROP TABLE IF EXISTS $TABLE_OPERATOR"
-        val dropTableExQuery = "DROP TABLE IF EXISTS $TABLE_EXERCISE"
-        val dropTableTrainingQuery = "DROP TABLE IF EXISTS $TABLE_TRAINING"
-        val dropTableTDQuery = "DROP TABLE IF EXISTS $TABLE_TRAININGDETAILS"
-        db?.execSQL(dropTableClientQuery)
-        db?.execSQL(dropTableRPRQuery)
-        db?.execSQL(dropTableOpQuery)
-        db?.execSQL(dropTableExQuery)
-        db?.execSQL(dropTableTrainingQuery)
-        db?.execSQL(dropTableTDQuery)
-        onCreate(db)
+        val dbFile = File(dbFilePath)
+        if (dbFile.exists()) {
+            dbFile.delete()
+        }
+        copyDatabaseIfNeeded()
+    }
+
+    private fun copyDatabaseIfNeeded() {
+        val dbFile = File(dbFilePath)
+        if (!dbFile.exists()) {
+            try {
+                val dbFolder = File(DATABASE_PATH)
+                if (!dbFolder.exists()) {
+                    dbFolder.mkdirs()
+                }
+                copyDatabaseFromAssets()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun copyDatabaseFromAssets() {
+        try {
+            val inputStream: InputStream = appContext.assets.open(DATABASE_NAME)
+            val outputStream: OutputStream = FileOutputStream(dbFilePath)
+
+            val buffer = ByteArray(1024)
+            var length: Int
+            while (inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+
+            outputStream.flush()
+            outputStream.close()
+            inputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun getWritableDatabase(): SQLiteDatabase {
+        return SQLiteDatabase.openDatabase(dbFilePath, null, SQLiteDatabase.OPEN_READWRITE)
+    }
+
+    override fun getReadableDatabase(): SQLiteDatabase {
+        return SQLiteDatabase.openDatabase(dbFilePath, null, SQLiteDatabase.OPEN_READONLY)
     }
 
     fun insertRPR(rprModel: RPRModel){
         val db = writableDatabase
         val values = ContentValues().apply{
-            put(COLUMN_RPR_DOMS, rprModel.doms)
-            put(COLUMN_RPR_ENERGY, rprModel.energy)
-            put(COLUMN_RPR_SLEEP, rprModel.sleep)
-            put(COLUMN_RPR_MOOD, rprModel.mood)
-            put(COLUMN_RPR_CLIENT, rprModel.clientID)
-            put(COLUMN_RPR_BORG, rprModel.borg)
-            put(COLUMN_RPR_AVG, rprModel.index)
-            put(COLUMN_RPR_TRAINING, rprModel.training)
+            put("doms", rprModel.doms)
+            put("energy", rprModel.energy)
+            put("sleep", rprModel.sleep)
+            put("mood", rprModel.mood)
+            put("client", rprModel.clientID)
+            put("borg", rprModel.borg)
+            put("avg", rprModel.index)
+            put("training", rprModel.training)
         }
-        db.insert(TABLE_RPR, null, values)
+        db.insert("rpr", null, values)
         db.close()
     }
+
 
     fun insertClient(clientModel: ClientModel){
         val db = writableDatabase
         val values = ContentValues().apply{
-            put(COLUMN_CLIENT_NAME, clientModel.name)
-            put(COLUMN_CLIENT_SURNAME, clientModel.surname)
+            put("name", clientModel.name)
+            put("surname", clientModel.surname)
         }
-        db.insert(TABLE_CLIENT, null, values)
+        db.insert("client", null, values)
         db.close()
     }
 
     fun insertExercise(exerciseModel: ExerciseModel){
         val db = writableDatabase
         val values = ContentValues().apply{
-            put(COLUMN_EX_NAME, exerciseModel.name)
-            put(COLUMN_EX_CATEGORY, exerciseModel.category)
-            put(COLUMN_EX_PHYSICAL_GOAL, exerciseModel.physicalGoal)
-            put(COLUMN_EX_TYPE, exerciseModel.type)
+            put("name", exerciseModel.name)
+            put("category", exerciseModel.category)
+            put("physicalGoal", exerciseModel.physicalGoal)
+            put("type", exerciseModel.type)
         }
-        db.insert(TABLE_EXERCISE, null, values)
+        db.insert("exercise", null, values)
         db.close()
     }
 
     fun insertTraining(training: TrainingModel){
         val db= writableDatabase
         val values = ContentValues().apply{
-            put(COLUMN_TD_ID, training.id)
-            put(COLUMN_TRAINING_DATE, training.date)
-            put(COLUMN_TRAINING_DURATION, training.duration)
-            put(COLUMN_TRAINING_CLIENT, training.clientId)
-            put(COLUMN_TRAINING_PT, training.personalTrainerId)
-            put(COLUMN_TRAINING_WNUMBER, training.workoutNumber)
+            put("id", training.id)
+            put("date", training.date)
+            put("duration", training.duration)
+            put("client", training.clientId)
+            put("operator", training.personalTrainerId)
+            put("workoutNumber", training.workoutNumber)
         }
-        db.insert(TABLE_TRAINING, null, values)
+        db.insert("training", null, values)
         db.close()
     }
 
     fun insertTrainingDetails(trainingDetails: TrainingDetailsModel){
         val db=writableDatabase
         val values = ContentValues().apply {
-            put(COLUMN_TD_REPS, trainingDetails.reps)
-            put(COLUMN_TD_SETS, trainingDetails.sets)
-            put(COLUMN_TD_WEIGHT, trainingDetails.weight)
-            put(COLUMN_TD_TRAINING, trainingDetails.trainingId)
-            put(COLUMN_TD_EXERCISE, trainingDetails.exerciseId)
-            put(COLUMN_TD_NOTE, trainingDetails.note)
-            put(COLUMN_TD_BORG, trainingDetails.borg.toString())
-            put(COLUMN_TD_EXTIME, trainingDetails.executionTime)
+            put("reps", trainingDetails.reps)
+            put("sets", trainingDetails.sets)
+            put("weight", trainingDetails.weight)
+            put("training", trainingDetails.trainingId)
+            put("exercise", trainingDetails.exerciseId)
+            put("note", trainingDetails.note)
+            put("borg", trainingDetails.borg.toString())
+            put("executionTime", trainingDetails.executionTime)
         }
-        db.insert(TABLE_TRAININGDETAILS, null, values)
+        db.insert("trainingDetails", null, values)
         db.close()
     }
 
     fun insertOperator(personalTrainerMode: PersonalTrainerModel){
         val db=writableDatabase
         val values = ContentValues().apply {
-            put(COLUMN_OP_NAME, personalTrainerMode.name)
-            put(COLUMN_OP_UID, personalTrainerMode.uid)
+            put("name", personalTrainerMode.name)
+            put("uid", personalTrainerMode.uid)
         }
-        db.insert(TABLE_OPERATOR, null, values)
+        db.insert("operator", null, values)
         db.close()
     }
 
@@ -315,7 +255,7 @@ class WorkoutDatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATAB
 
     fun countTrainingRows(): Int{
         val db = readableDatabase
-        val query = "SELECT COUNT(*) FROM $TABLE_TRAINING"
+        val query = "SELECT COUNT(*) FROM training"
         val cursor = db.rawQuery(query, null)
         var count=0
         if(cursor.moveToFirst()){
@@ -328,16 +268,16 @@ class WorkoutDatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATAB
     fun getTrainingByClientId(id: String): List<TrainingModel> {
         val trainingList = mutableListOf<TrainingModel>()
         val db = readableDatabase
-        val query = "SELECT * FROM $TABLE_TRAINING WHERE client=?"
+        val query = "SELECT * FROM training WHERE client=?"
         val cursor = db.rawQuery(query, arrayOf(id))
         while (cursor.moveToNext()) {
             val training = TrainingModel(
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_ID)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_DATE)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_DURATION)),
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_CLIENT)).toString(),
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_PT)).toString(),
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TRAINING_WNUMBER))
+                cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("date")),
+                cursor.getString(cursor.getColumnIndexOrThrow("duration")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("client")).toString(),
+                cursor.getInt(cursor.getColumnIndexOrThrow("operator")).toString(),
+                cursor.getInt(cursor.getColumnIndexOrThrow("workoutNumber"))
             )
             trainingList.add(training)
         }
@@ -367,20 +307,20 @@ class WorkoutDatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATAB
 
     fun getRprByTrainingId(trainingId: String): RPRModel? {
         val db = readableDatabase
-        val query = "SELECT * FROM $TABLE_RPR WHERE training=?"
+        val query = "SELECT * FROM rpr WHERE training=?"
         var rprModel: RPRModel? = null
         val cursor = db.rawQuery(query, arrayOf(trainingId))
         if (cursor.moveToFirst()) {
             rprModel = RPRModel(
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_RPR_ID)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_CLIENT)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_MOOD)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_SLEEP)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_ENERGY)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_DOMS)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_AVG)),
-                cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_RPR_BORG)),
-                cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_RPR_TRAINING))
+                cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("client")),
+                cursor.getString(cursor.getColumnIndexOrThrow("mood")),
+                cursor.getString(cursor.getColumnIndexOrThrow("sleep")),
+                cursor.getString(cursor.getColumnIndexOrThrow("energy")),
+                cursor.getString(cursor.getColumnIndexOrThrow("doms")),
+                cursor.getString(cursor.getColumnIndexOrThrow("avg")),
+                cursor.getString(cursor.getColumnIndexOrThrow("borg")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("training"))
             )
         }
         cursor.close()
